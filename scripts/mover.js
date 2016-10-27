b5e2857a3d3fadfc971b6076b909de9b1543c2a1
deworkers <z-be@yandex.ru>
2016-10-28 02:44:40 +0500

    finish

--- scripts/mover.js
+++ scripts/mover.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-	
+	$('.header__mobile-menu').click(function() {
+		$(this).next().slideToggle();
+		$('.header__mobile-menu').toggleClass('header__mobile-menu--open');
+	});
+
+
 
 	
 });
